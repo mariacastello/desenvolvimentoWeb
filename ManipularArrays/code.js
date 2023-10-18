943928c25f0e4de8d223ae1a63e548f68481e644
Maria <62152408+mariacastello@users.noreply.github.com>
2023-10-18 12:02:29 +0000

    Update code.js

--- ManipularArrays/code.js
+++ ManipularArrays/code.js
@@ -1,20 +1,37 @@
-const ingrid = atletas [0];
+const body = document.body;
+const div_container = document.createElement('div');
+div_container.id = 'container';
+body.appendChild(div_container);
 
-const container = document.getElementById('container');
-const espaco_nome = document.getElementById('nome');
-const imagem = document.querySelector('div#container > img');
-const espaco_descricao = document.querySelector('div#container > p');
+const cria_cartao = (atleta) => {
 
-const preenche = (atleta) => {
+    const container_atleta = document.createElement('div');
+    const titulo = document.createElement('h3');
+    titulo.innerHTML = atleta.nome;
+    const imagem = document.createElement('img');
+    imagem.src = atleta.imagem;
+    const descricao = document.createElement('p');
+    descricao.innerHTML = atleta.descricao;
+
+    container_atleta.appendChild(titulo);
+    container_atleta.appendChild(imagem);
+    container_atleta.appendChild(descricao);
+
+    div_container.appendChild(container_atleta);
+}
+
+container.style.width = '15rem';
+container.style.color = 'white';
+container.style.backgroundColor = 'black';
+
+atletas.forEach((atleta) => {cria_cartao(atleta)});
+
+/*const preenche = (atleta) => {
 
     espaco_nome.innerText = atleta.nome;
     imagem.src = atleta.imagem
     espaco_descricao.innerText = atleta.descricao
 
-    container.style.width = '15rem';
-    container.style.color = 'white';
-    container.style.backgroundColor = 'black';
-
 }
 
-preenche(atletas[5]);
+preenche(atletas[5]);*/
